973b13bc1ed4229117ccec0315c264709d519870
amorimlt <julianeamorim.lt@gmail.com>
2022-03-18 20:34:26 -0300

    finaliza exercicio

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿string frase, palavracensurada = "#@$%*!&";
+
+Console.Write("Digite uma frase: ");
+frase = Console.ReadLine()!;
+
+frase = frase
+    .Replace("bobo", $"{palavracensurada}")
+    .Replace("boba", $"{palavracensurada}")
+    .Replace("feio", $"{palavracensurada}")
+    .Replace("feia", $"{palavracensurada}")
+    .Replace("estranho", $"{palavracensurada}")
+    .Replace("estranha", $"{palavracensurada}")
+    .Replace("esquisito", $"{palavracensurada}")
+    .Replace("esquisita", $"{palavracensurada}")
+    .Replace("panaca", $"{palavracensurada}")
+    .Replace("chato", $"{palavracensurada}")
+    .Replace("chata", $"{palavracensurada}");
+
+Console.WriteLine($"{frase}");
